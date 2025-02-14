ba3c3fc09284f7c6f88a605d6a70089e34151521
reledge <reledge676@gmail.com>
2025-02-14 15:36:59 +0200

    LELELEL

---
+++ II semestric/14-02-2025/script.js
@@ -0,0 +1,53 @@
+//alert ("čau čau te raiders raitis");
+
+
+
+/*
+let x = 5;
+y = 4;
+//alert(x);
+console.log("x+y=",x*y);
+console.log("x+y=",x+y);
+console.log("x+y=",x/y);
+console.log("x+y=",x-y);
+console.log("x+y=",x**y);
+*/
+//let number = prompt("ierakst skaitli skaidrs?")
+
+/*
+
+if (number > 0)
+{
+    console.log(number, "ir pozitīvs");
+}
+if (number < 0)
+    {
+        console.log(number, "ir negatīvs");
+    }
+if (number == 0)
+     {
+         console.log(number, "ir 0 dumiķi the fuck?");
+     }
+*/
+
+
+
+
+let age = prompt("ierakst vecumu bossman skaidrs?")
+let citizenship = false;
+if (age >= 18 && citizenship)
+{
+    console.log("var balsot")
+}
+if(age < 18 && citizenship)
+{
+    console.log("ir pilsons bet nav 18, pieaudz puika")
+}    
+if (age >= 18 && !citizenship)
+{
+    console.log("nu liels berns bet nahuj neesi latvietis")
+}
+if (age < 18 && !citizenship)
+    {
+        console.log("maziņs bebītis kurš nav latvietits un tu doma ka vari balsot?")
+    }
