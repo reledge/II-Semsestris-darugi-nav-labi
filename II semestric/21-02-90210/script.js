4dc8f409deb60108d258c42f85e147312bec0fc3
kr1stena <reledge676@gmail.com>
2025-02-21 12:36:04 +0200

    ja ja ja

---
+++ II semestric/21-02-90210/script.js
@@ -0,0 +1,82 @@
+/*
+let gadalaiks = prompt("ieraksitti menesi PAREIZI pirmais bruts vajag būt MAZAIS. skaidrs?")
+
+switch (gadalaiks) {
+    case "decembris":
+        console.log("Šis mēnesis ir ziemā fujak");
+        break;
+    case "janvāris":
+        console.log("Šis mēnesis ir ziemā fujak.");
+        break;
+    case "februāris":
+        console.log("Šis mēnesis ir ziemā fujak");
+        break;
+    default:
+        console.log("nah, nav ziema fujak");
+}
+*/
+/*
+let diena = prompt("ieraksitti dienu PAREIZI pirmais bruts vajag būt MAZAIS. skaidrs?")
+
+switch (diena) {
+    case "pirmdiena":
+        console.log("ej uz darbu žuļik");
+        break;
+    case "otrdiena":
+        console.log("ej uz darbu žuļik");
+        break;
+    case "trešdiena":
+        console.log("ej uz darbu žuļik");
+        break;
+        case "ceturtdiena":
+        console.log("ej uz darbu žuļik");
+        break;
+        case "piektdiena":
+        console.log("ej uz darbu žuļik");
+        break;
+    default:
+        console.log("lucky day, nav jāstrādā");
+}
+*/
+/*
+let atzīme = prompt("ieraksitti dienu PAREIZI pirmais bruts vajag būt MAZAIS. skaidrs?")
+
+switch (atzīme) {
+    case "10":
+    case "9":
+    case "8":
+        console.log("labais, laba atzīme");
+        break;
+    case "7":
+    case "6":
+    case "5":
+        console.log("nuuu jau sliktāk, pamacies žuļik, ļoti vidēji");
+        break;
+    case "4":
+    case "3":
+    case "2":
+    case "1":
+        console.log("JOPCIK! NU GAN TU NORAUSIES!");
+}
+*/
+
+let currency = prompt("ieraksti naudu ar 3 lieliem burtiem bet nuu... lūdzu angļu eiropa vai asv naudu...")
+
+switch (currency) {
+    case "GBP":
+        console.log("tas tas angliskais nauda");
+        break;
+    case "USD":
+        console.log("tas ir tas amerikan dolarins");
+        break;
+    case "EUR":
+        console.log("tas ir jevriks");
+    default:
+        console.log("eu nu.. es teicu tikai anglu, asv vai eiropas..")
+}
+
+
+
+
+
+
